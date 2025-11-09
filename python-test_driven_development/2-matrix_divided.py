802061bd02c6b965c5a00119dba7e97b779e5b8e
safarxe <github@subs.az>
2025-11-09 22:56:35 +0400

    Fix matrix_divided for infinity and missing args tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,16 +7,15 @@ This is the "matrix_divided" module.
 def matrix_divided(matrix, div):
     """Return a new matrix with divided and rounded values."""
 
-    if not isinstance(matrix, list) or not all(isinstance(r, list) for r in matrix):
+    if (not isinstance(matrix, list) or
+            not all(isinstance(r, list) for r in matrix) or
+            not all(isinstance(n, (int, float)) for r in matrix for n in r)):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats"
         )
 
-    if not matrix or not all(isinstance(n, (int, float)) for r in matrix for n in r):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-
+    if len(matrix) == 0:
+        raise TypeError("Each row of the matrix must have the same size")
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
@@ -24,5 +23,6 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
     return (new_matrix)
