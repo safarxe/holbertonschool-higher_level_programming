90b405139df3be73145d1a9b98e26f728ced00fb
safarxe <github@subs.az>
2025-11-16 23:33:12 +0400

    add string representation

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""rectangle class with string representation"""
+
+
+class Rectangle:
+    """rectangle class with string representation"""
+
+    def __init__(self, width=0, height=0):
+        """initialize rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculate rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """calculate rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """return string representation"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        result = []
+        for i in range(self.__height):
+            result.append("#" * self.__width)
+        return "\n".join(result)
+
