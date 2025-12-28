3b5fb896ae40b517e6049203a8207c8893ff78cf
safarxe <github@subs.az>
2025-12-28 12:32:19 -0500

    add js warm up task

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2).map(x => parseInt(x));
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  const sorted = args.sort((a, b) => b - a); // böyükdən kiçiyə sort
+  console.log(sorted[1]);
+}
